ff7360ffa9c867b52aae4b721e0f8f5b4899a8da
bpavlakos-edu <pav17002@byui.edu>
2023-01-05 11:38:53 -0800

    Finished CSharp Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
+//https://byui-cse.github.io/cse210-course-2023/unit01/csharp-1.html
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+        string firstName = input("What is your first name? ");
+        string lastName = input("What is your last name? ");
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+
+    //Function to replicate python style input, to reduce repeated code, this also allows for protections in the future too
+    static string input(string inMsg){
+        Console.Write(inMsg);
+        return Console.ReadLine();
     }
 }
