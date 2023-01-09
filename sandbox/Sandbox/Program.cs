600ef386ef6889352152772b8c0d7af6933686ad
bpavlakos-edu <pav17002@byui.edu>
2023-01-09 13:36:09 -0800

    Added placeholders to sandbox for future code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,4 +6,19 @@ class Program
     {
         Console.WriteLine("Hello Sandbox World!");
     }
+
+    //Testing for a later project
+    static void testBeep(){
+        Console.Beep();
+        //[Final Project Idea] will be tested here in the future
+    }
+
+    //Will be tested later unless foreach supports tracking the index
+    static void forEachIndexLoopTest(){
+
+    }
+
+
+
+
 }
