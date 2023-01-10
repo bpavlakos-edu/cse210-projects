e66efb9bae902be9a87e70bedf151add46ea87da
bpavlakos-edu <pav17002@byui.edu>
2023-01-09 16:41:35 -0800

    Finished C# Prep 5 (More info coming soon)

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(),SquareNumber(PromptUserNumber()));
+    }
+
+
+    //Required Functions
+
+    //"DisplayWelcome - Displays the message, "Welcome to the Program!""
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+    //PromptUserName - Asks for and returns the user's name (as a string)
+    static string PromptUserName()
+    {
+        return GetInput("Please enter your name:");
+    }
+    //PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber()
+    {
+        while (1 == 1)
+        {
+            try
+            {
+                return int.Parse(GetInput("Please enter your favorite number:"));
+            }
+            catch
+            {
+                Console.WriteLine("That is not a valid whole number, please try again!");
+            }
+        }
+    }
+    //SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    static int SquareNumber(int inNum)
+    {
+        return inNum * inNum;
+    }
+    //DisplayResult - Accepts the user's name and the squared number and displays them.
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your favorite number is {squaredNumber}");
+    }
+
+    //Additional Functions
+    //The core function to get user input
+    static string GetInput(string inMsg)
+    {
+        Console.Write(inMsg+" ");//Add the space by default to prevent redunant work
+        return Console.ReadLine();
     }
 }
