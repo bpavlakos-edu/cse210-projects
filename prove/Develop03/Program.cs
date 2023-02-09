97c0fc29cb2e269a18eb49444f5a36b955ac751c
bpavlakos-edu <pav17002@byui.edu>
2023-02-08 17:12:46 -0800

    Verified it's working, added documentation

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     public static Scripture myScripture = new Scripture("Mosiah 2:17","And behold, I tell you these things that ye may learn wisdom; that ye may learn that when ye are in the service of your fellow beings ye are only in the service of your God.");
     static void Main(string[] args)
     {
-        UiLoop();
+        UiLoop(); //Trigger the main UI loop
     }
     static void UiLoop()
     {
@@ -13,7 +13,7 @@ class Program
         {
             //Display sequence
             Console.Clear(); //Clear the console
-            myScripture.Display();
+            myScripture.Display(); //Trigger the scripture display function
             Console.WriteLine();//Blank Line
             string userInput = GetInputLine("Press enter to continue, type 'reset' to restart, or type 'quit' to finish:");
             if(userInput == "quit")
@@ -22,7 +22,7 @@ class Program
             }
             else if(userInput == "reset")
             {
-                myScripture.Reset();
+                myScripture.Reset(); //Reset the scripture
             }
             else
             {
@@ -38,7 +38,6 @@ class Program
                     {
                         myScripture.Reset(); //Reset the scripture
                     }
-
                 }
             }
         }
@@ -46,12 +45,12 @@ class Program
     //New line function overload created to match original project video (https://byui-cse.github.io/cse210-course-2023/unit03/develop.html)
     public static string GetInputLine(string inMsg="", bool returnLowerCase = true)
     {
-        return GetInput(inMsg+Environment.NewLine, returnLowerCase);
+        return GetInput(inMsg+Environment.NewLine, returnLowerCase); //Append the system newline to the message
     }
     public static string GetInput(string inMsg="", bool returnLowerCase = true)
     {
-        Console.Write(inMsg);
-        string userInput = Console.ReadLine();
+        Console.Write(inMsg); //Write the message
+        string userInput = Console.ReadLine(); //Get the user input
         if(returnLowerCase)
         {
             return userInput.ToLower(); //Return lower case input
@@ -62,6 +61,6 @@ class Program
     static bool ConfirmExit()
     {
         string userInput = GetInput("Would you like to quit? (enter 'y' or 'quit' to finish, enter anything else to restart): ");
-        return  userInput == "y" || userInput == "quit";
+        return  userInput == "y" || userInput == "quit"; //Return if the user input matches any of the exit keywords
     }
 }
