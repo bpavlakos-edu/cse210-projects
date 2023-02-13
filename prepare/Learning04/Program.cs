a457c5624b5a3f0a56093ee5162619968ed80949
bpavlakos-edu <pav17002@byui.edu>
2023-02-13 13:30:11 -0800

    Starting Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,11 @@
+//Assignment Specifications: https://byui-cse.github.io/cse210-course-2023/unit04/prepare.html
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
     }
 }
