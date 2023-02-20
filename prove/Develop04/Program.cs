ccbf355cd5abe246b2bf7e5693f90f7b883f2230
bpavlakos-edu <pav17002@byui.edu>
2023-02-20 12:52:19 -0800

    Created initial files

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,24 @@
+//Mindfullness program
+//Week 08
+
 using System;
 
 class Program
 {
+    //BreathingActivity bAct = new BreathingActivity();
+    //ReflectionActivity rAct = new ReflectionActivity();
+    //ListingActivity lAct = new ListingActivity();
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        UiLoop();    
+    }
+    static void UiLoop()
+    {
+
+    }
+    static string GetInput(string inMsg)
+    {
+        Console.Write(inMsg);
+        return Console.ReadLine();
     }
 }
