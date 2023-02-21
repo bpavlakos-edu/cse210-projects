91e3e35358d650db2deb3a022943993991291eaa
bpavlakos-edu <pav17002@byui.edu>
2023-02-20 19:58:00 -0800

    Added Reflection Getters and Setters

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -11,7 +11,17 @@ class ReflectionActivity : Activity
     //Fill All Attributes
     public ReflectionActivity(string name, string description, List<string>messageList, int pauseStyle, List<string> refQuestions):base(name, description, messageList, pauseStyle)
     {
-        _refQuestions = refQuestions.ToList<string>();
+        _refQuestions = refQuestions.ToList<string>(); //Use toList to erase the reference to the original list
+    }
+
+    //Getters and setters
+    public List<string> GetRefQuestions()
+    {
+        return _refQuestions.ToList<string>(); //Use toList to erase the reference to the original list
+    }
+    public void SetRefQuestions(List<string> refQuestions)
+    {
+        _refQuestions = refQuestions.ToList<string>(); //Use toList to erase the reference to the original list
     }
 
 }
