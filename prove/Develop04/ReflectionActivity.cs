4daa8c735771742b3b8f2cdb9444238e307b6a0d
bpavlakos-edu <pav17002@byui.edu>
2023-02-20 20:34:35 -0800

    Completed Reflection Activity Loop

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -31,16 +31,39 @@ class ReflectionActivity : Activity
         Console.WriteLine("");
         Console.WriteLine($" --- {GetRandomMsg(_messageList)} ---"); //Use GetRandomMessage to pick the prompt
         Console.WriteLine("");
-        //Wait for user input
+
+        //Wait for user input to continue
         GetInput("When you have something in mind, press enter to continue.");
         Console.WriteLine("");
-        
+
+        //Prepare for main loop
         Console.WriteLine("Now ponder on each of the following questions as they are related to this experience.");
         Console.Write("You may begin in: ");
         Pause(5000,1); //Countdown timer
+        Console.Clear(); //Clear the console
 
-        Console.Clear();
-
+        long[] tickTimes = GetTickStartEnd(durationMsec); //Get the start and end times
+        long curTime = tickTimes[0]; //Initalize curTime to the start time
+        
+        List<string> usedPrompts = new List<string>(); //List to store the used prompts
+        int cyclePauseMsec = 10 * 1000; //each prompt should get a 10 second pause
+        bool lastCycle = false;
+        while (curTime < tickTimes[1])
+        {
+            if(curTime + (cyclePauseMsec * 10000) >= tickTimes[1])//Check if this is the last cycle
+            {
+                lastCycle = true;
+                Console.WriteLine("");
+            }
+            //Display the prompt
+            string curPrompt = GetRandomMsg(_refQuestions, usedPrompts); //Get a random question
+            Console.Write($"> {curPrompt} ");
+            //Console.Write($"> {GetRandomMsg(_refQuestions, usedPrompts)} ");//Single Line version
+            Pause(cyclePauseMsec, 0);//Spinner pause type
+            if(!lastCycle)
+            {
+                Console.WriteLine("");//Only print a line on every cycle except the last
+            }
+        }
     }
-
 }
