2dc42543054075a2c08085aa52641f9b823c25d1
bpavlakos-edu <pav17002@byui.edu>
2023-02-20 20:10:45 -0800

    Adjusted code formatting

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,18 +5,30 @@ using System;
 
 class Program
 {
-    static BreathingActivity bAct = new BreathingActivity("Breathing Activity","This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.",new List<string>{"Breathe in...","Breathe out..."},1);
+    //Global Activity Class Setup
+    static BreathingActivity bAct = new BreathingActivity(
+        "Breathing Activity",
+        "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.",
+        new List<string>
+        {
+            "Breathe in...",
+            "Breathe out..."
+        },
+        1
+    );
     static ReflectionActivity rAct = new ReflectionActivity(
         "Reflection Activity",
         "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",
-        new List<string>{
+        new List<string>
+        {
             "Think of a time when you stood up for someone else.",
             "Think of a time when you did something really difficult.",
             "Think of a time when you helped someone in need.",
             "Think of a time when you did something truly selfless."
         }, 
         0, 
-        new List<string>{
+        new List<string>
+        {
             "Why was this experience meaningful to you?",
             "Have you ever done anything like this before?",
             "How did you get started?",
