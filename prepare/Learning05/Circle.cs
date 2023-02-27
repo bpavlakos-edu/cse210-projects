5ef3f534ab12ac8cbaa9e3fa194f156fa3dd86d9
bpavlakos-edu <pav17002@byui.edu>
2023-02-27 12:11:48 -0800

    Finished circle subclass

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -1,4 +1,24 @@
+//Circle Subclass
 class Circle : Shape
 {
+    private double _radius;
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
 
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * (_radius * _radius); //Pi*r^2
+    }
 }
