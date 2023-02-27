765da679574408aa15e4749643e463c5878e6a1b
bpavlakos-edu <pav17002@byui.edu>
2023-02-27 12:03:41 -0800

    Finished Square Sub-Class

--- prepare/Learning05/Square.cs
+++ prepare/Learning05/Square.cs
@@ -1,4 +1,24 @@
+//Square sub-class
 class Square : Shape
 {
+    private double _side;
 
+    public Square(double side)
+    {
+        _side = side;
+    }
+
+    public double GetSide()
+    {
+        return _side;
+    }
+    public void SetSide(double side)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
 }
