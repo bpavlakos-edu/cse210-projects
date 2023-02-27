a5ffab0168045136b9f197b0f747e82491b82848
bpavlakos-edu <pav17002@byui.edu>
2023-02-27 12:07:50 -0800

    Finished Rectangle subclass

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -1,4 +1,38 @@
+//Rectangle sub class
 class Rectangle : Shape
 {
+    private double _length;
+    private double _width;
+
+    public Rectangle(double length, double width)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public double GetLength()
+    {
+        return _length;
+    }
+
+    public void SetLength(double length)
+    {
+        _length = length;
+    }
+
+    public double GetWidth()
+    {
+        return _width;
+    }
+
+    public void SetWidth(double width)
+    {
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
 
 }
