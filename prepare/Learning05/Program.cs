f3a7cab37da5225c1e05741effd741a55e28da05
bpavlakos-edu <pav17002@byui.edu>
2023-02-27 12:32:03 -0800

    Finished Step 7

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,10 +6,21 @@ class Program
     static void Main(string[] args)
     {
         //Step 5: Test the square class
-        Square squareTest = new Square("Red",5);
+        //Square squareTest = new Square("Red",5.5);
+        /*
         Console.WriteLine($"Area of a 5 by 5 square: {squareTest.GetArea()}");
         squareTest.SetSide(3.5);
         Console.WriteLine($"Area of a 3.5 by 3.5 square: {squareTest.GetArea()}");
+        Console.WriteLine("");
+        */
+        //Step 7: Test Rectangle and Circle using a list of shape
 
+        List<Shape> shapeList = new List<Shape>{new Square("Red",5.5), new Rectangle("Blue",2.5,11), new Circle("Green",5)};
+        //foreach(Shape curShape in shapeList){};//For each loop
+        //For loop
+        for(int i = 0; i < shapeList.Count; i++)
+        {
+            Console.WriteLine($"Shape {i+1}, Color: {shapeList[i].GetColor()} Area: {shapeList[i].GetArea()}");
+        }
     }
 }
