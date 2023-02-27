3de783c3dc544eb5bfd788fb865323e5892d7d10
bpavlakos-edu <pav17002@byui.edu>
2023-02-27 12:00:05 -0800

    Finished Shape Abstract Super Class

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,4 +1,16 @@
-class Shape
+//Shape Abstract Super-Class
+abstract class Shape
 {
-    
+    private string _color;
+
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
 }
