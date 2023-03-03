993461bbd6daee8c865f1c3d9a05278d8197690e
bpavlakos-edu <pav17002@byui.edu>
2023-03-02 18:14:43 -0800

    Finished JSON support

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 class GoalManager
 {
     //Attributes
@@ -100,14 +102,99 @@ class GoalManager
         //_points += _goalList[goalIndex].Mark();
     }
 
-    //File Handling
+    //File Management
     public void Save(string filePath)
     {
-
+        try
+        {
+            //I found File.CreateText() using intellisense, it creates the file if missing, or erases it before writing
+            using(StreamWriter sWriter = File.CreateText(filePath))
+            {
+                string jsonString = ToJson(); //Generate the JSON string if possible
+                if(jsonString != "")
+                {
+                    sWriter.Write(jsonString); //Write the JSON string
+                    Console.WriteLine("File successfuly saved");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to save file, JSON text was empty!");
+                }
+            }
+        }
+        catch(IOException e) //IO Errors
+        {
+            Console.WriteLine($"Unable to create file, error: {e.ToString()}");
+        }
+        catch(NotSupportedException){}
+        catch(ObjectDisposedException){}
+        catch(ArgumentException){}
+        //catch(ArgumentNullException){} //Caught by ArgumentException
     }
     public void Load(string filePath)
     {
+        try
+        {
+            string jsonText = "";
+            using(StreamReader sReader = File.OpenText(filePath))
+            {
+                jsonText = sReader.ReadToEnd(); //Read all file lines
+            }
+            FromJson(jsonText); //Use the FromJson method, which will update the global manager
+        }
+        catch(IOException e) //IO Errors
+        {
+            Console.WriteLine($"Unable to load file, error: {e.ToString()}");
+        }
+        //Other errors
+        catch(NotSupportedException){}
+        catch(ObjectDisposedException){}
+        catch(ArgumentException){}
+        //catch(ArgumentNullException){} //Caught by ArgumentException
+    }
 
+    //JSON Helpers
+    private string ToJson()
+    {
+        try
+        {
+            return JsonSerializer.Serialize<GoalManager>(this, new JsonSerializerOptions{IncludeFields = true, WriteIndented = true})??"";
+        }
+        catch(NotSupportedException)
+        {
+            Console.WriteLine("Json is not supported on this computer!");
+            return "";
+        }
+        
+    }
+    private void FromJson(string jsonText)
+    {
+        if(jsonText != "")
+        {
+            try
+            {
+                GoalManager newGoalManager = JsonSerializer.Deserialize<GoalManager>(jsonText, new JsonSerializerOptions{IncludeFields = true, WriteIndented = true});
+                _goalList = newGoalManager.GetGoalList();
+                _points = newGoalManager.GetPoints();
+                //_name = newGoalManager.GetName();
+            }
+            catch(JsonException e)
+            {
+                Console.WriteLine($"Error, {e.ToString()}");
+            }
+            catch(NotSupportedException e)
+            {
+                Console.WriteLine($"Incompatible file! {e.ToString()}");
+            }
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine($"Error, file was empty! {e.ToString()}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error, file was empty!");
+        }
     }
 
     //User Input
@@ -134,7 +221,7 @@ class GoalManager
                     }
                     else if(max < min && returnVal >= min) //When max < min it's Minimum only
                     {
-
+                        return returnVal;
                     }
                     /*else if(maximumOnly)
                     {
