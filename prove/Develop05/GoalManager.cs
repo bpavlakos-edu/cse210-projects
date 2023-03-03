a97c7f3b2613dbd887b04cc14d94e1144fe873b1
bpavlakos-edu <pav17002@byui.edu>
2023-03-02 16:47:13 -0800

    Finished placeholders for UI options 1 and 2

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -47,4 +47,37 @@ class GoalManager
     {
         _points = points;
     }
+    //GetName
+    //SetName
+
+    //Methods
+    //Main Display
+    public void Display()
+    {
+        //Consider using UI Menu here (Programically generate options)
+        Console.WriteLine("");
+        for(int i = 0; i < _goalList.Count; i++)
+        {
+            //Console.WriteLine(_goalList[i].ToDisplayString(i));
+        }
+    }
+    //Goal List Modification
+    public void NewGoal()
+    {
+        //_goalList.Add(new Goal(0));
+    }
+    public void DeleteGoal()
+    {
+        //Consider using UI Menu Here
+        //Int input (index of goal in list), invalid to cancel
+        //_goalList.RemoveAt(index);
+    }
+    public void EditGoal()
+    {
+        //Consider using UI Menu Here
+        //Int input (index of goal in list), invalid to cancel
+        //Int input (index of property to change), invalid to cancel
+        //input / int input, new value [Write new value]
+    }
+    //File loading and saving
 }
