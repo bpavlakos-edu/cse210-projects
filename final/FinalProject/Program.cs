2df059ac510e9b75ff0f215caa1ed57825e84179
bpavlakos-edu <pav17002@byui.edu>
2023-03-20 09:26:01 -0700

    Added UiMenu

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,18 @@
 using System;
-
+using UiMenu = QuickUtils.UiMenu;
+using UiOption = QuickUtils.UiOption;
 class Program
 {
+    public static UiMenu _mainMenu = new UiMenu(new List<UiOption>{
+            new UiOption(()=>{},"&Classic"),
+            new UiOption(()=>{},"&Random"),
+            new UiOption(()=>{},"&Blink"),
+            new UiOption(()=>{},"&Options"),
+            new UiOption(()=>{throw new OperationCanceledException();},"&Exit")
+        }
+    );
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        _mainMenu.UiLoop(); //Open the main menu
     }
 }
