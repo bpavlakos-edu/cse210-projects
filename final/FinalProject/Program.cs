8f2b2bfaa62ce678f29fc883dd1c87cd3cac9a2e
bpavlakos-edu <pav17002@byui.edu>
2023-03-20 09:43:28 -0700

    Added Some Menu Functionality

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -3,16 +3,34 @@ using UiMenu = QuickUtils.UiMenu;
 using UiOption = QuickUtils.UiOption;
 class Program
 {
-    public static UiMenu _mainMenu = new UiMenu(new List<UiOption>{
+    //Global Variables
+    //private List<GameMode> _gameModeList = new List<GameMode>();
+    //UiMenu setup
+    private static UiMenu _mainMenu = new UiMenu(new List<UiOption>{
             new UiOption(()=>{},"&Classic"),
             new UiOption(()=>{},"&Random"),
             new UiOption(()=>{},"&Blink"),
-            new UiOption(()=>{},"&Options"),
+            new UiOption(OptionsMenu,"&Options"),
             new UiOption(()=>{throw new OperationCanceledException();},"&Exit")
-        }
+        },
+        $"Welcome to Doggle!{Environment.NewLine}Menu Options:",
+        "Select a Game Mode or [hotkey] from the menu: ",
+        "Thank you for playing!"
+        //Use default settings for the other 
     );
     static void Main(string[] args)
     {
         _mainMenu.UiLoop(); //Open the main menu
+    }
+    static void OptionsMenu()
+    {
+        UiMenu _rootOptionsMenu = new UiMenu(new List<UiOption>{
+            new UiOption(()=>{},"&Game Mode Options"),
+            new UiOption(()=>{},"&Dice Set Options"),
+            new UiOption(()=>{throw new OperationCanceledException();},"Go &Back"),
+        },
+        "Options:",
+        "Select an option or [hotkey] from the menu: "
+        );
     }
 }
