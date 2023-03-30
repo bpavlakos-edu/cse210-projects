4041b584b4a42c0725e111473be2f44e8973af35
bpavlakos-edu <pav17002@byui.edu>
2023-03-30 15:38:18 -0700

    Changed Bool constructor to have a better string

--- final/FinalProject/quickutils/UiOption.cs
+++ final/FinalProject/quickutils/UiOption.cs
@@ -69,8 +69,8 @@ namespace QuickUtils
         public UiOption(Func<bool> getterFun, Action<bool> setterFun, string hotkeyVarName, char yesChar='y', char noChar='n', bool newLine = false)
         {
             //This is quite complex so I'll do it in the constructor body
-            _runAction = () => {setterFun(Inputs.GetBoolInput($"What would you like to change {hotkeyVarName.Replace("&","").ToLower()} to?: ", yesChar, noChar, newLine, getterFun())); /*throw new OperationCanceledException();*/};
-            _dispStr = MakeDisplayName("Change "+hotkeyVarName);
+            _runAction = () => {setterFun(Inputs.GetBoolInput($"What would you like to enable {hotkeyVarName.Replace("&","").ToLower()} to?: ", yesChar, noChar, newLine, getterFun())); /*throw new OperationCanceledException();*/};
+            _dispStr = MakeDisplayName("Enable or disable "+hotkeyVarName);
             _hotkey = MakeHotkey(hotkeyVarName);
             _updateStrFun = () => {return " (Currently: "+Misc.BoolStr(getterFun())+")";};
         }
